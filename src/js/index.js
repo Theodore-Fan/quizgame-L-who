494a2e34c3be030071f4c8352f517b21dc265522
IvanHu203 <71461384+IvanHu203@users.noreply.github.com>
2021-03-24 16:29:24 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-console.log("connected");
+const question = {
+  questionContent: "",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "",
+      correct: true,
+      value: 1,
+    },
+    {
+      choiceContent: "",
+      correct: true,
+    },
+  ],
+};
+
+// Start event or on page load display quiz
+
+// Display question/questions
+
+// Listen for user click event next button or submit
+
+// If answer is true then update score, give user positive feedback modal
+// Else update score negative, give user negative feedback modal
+
+// Remove current question
+
+// If question number is less than question.length
+// Else display results
