c5f5bc6a63cb2c51d5ce3ab29ef6837a32fbd2bb
IvanHu203 <71461384+IvanHu203@users.noreply.github.com>
2021-03-25 23:17:11 -0400

    trying out a website

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,119 @@
-const question = {
-  questionContent:
-    "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
-  value: 1,
-  choices: [
-    {
-      choiceContent: "Tripartite Pact",
-      correct: false,
+var myQuestions = [
+  {
+    question: "What is 10/2?",
+    answers: {
+      a: "3",
+      b: "5",
+      c: "115",
     },
-    {
-      choiceContent: "Agreement of Mutual Assistance",
-      correct: false,
+    correctAnswer: "b",
+  },
+  {
+    question: "What is 30/3?",
+    answers: {
+      a: "3",
+      b: "5",
+      c: "10",
     },
-    {
-      choiceContent: "Treaty of Versailles",
-      correct: false,
-    },
-    {
-      choiceContent: "Molotov-Ribbentop Pact",
-      correct: true,
-      value: 1,
-    },
-  ],
-};
+    correctAnswer: "c",
+  },
+];
 
-// Start event or on page load display quiz
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
 
-// Display question/questions
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
-// Listen for user click event next button or submit
+function generateQuiz(
+  questions,
+  quizContainer,
+  resultsContainer,
+  submitButton
+) {
+  function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
+    var output = [];
+    var answers;
 
-// If answer is true then update score, give user positive feedback modal
-// Else update score negative, give user negative feedback modal
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+      // first reset the list of answers
+      answers = [];
 
-// Remove current question
+      // for each available answer...
+      for (letter in questions[i].answers) {
+        // ...add an html radio button
+        answers.push(
+          "<label>" +
+            '<input type="radio" name="question' +
+            i +
+            '" value="' +
+            letter +
+            '">' +
+            letter +
+            ": " +
+            questions[i].answers[letter] +
+            "</label>"
+        );
+      }
 
-// If question number is less than question.length
-// Else display results
+      // add this question and its answers to the output
+      output.push(
+        '<div class="question">' +
+          questions[i].question +
+          "</div>" +
+          '<div class="answers">' +
+          answers.join("") +
+          "</div>"
+      );
+    }
+
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults(questions, quizContainer, resultsContainer) {
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    var userAnswer = "";
+    var numCorrect = 0;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+      // find selected answer
+      userAnswer = (
+        answerContainers[i].querySelector(
+          "input[name=question" + i + "]:checked"
+        ) || {}
+      ).value;
+
+      // if answer is correct
+      if (userAnswer === questions[i].correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[i].style.color = "lightgreen";
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[i].style.color = "red";
+      }
+    }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
+  }
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
+
+  // on submit, show results
+  submitButton.onclick = function () {
+    showResults(questions, quizContainer, resultsContainer);
+  };
+}
