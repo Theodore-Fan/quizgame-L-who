0a7461595f362abc00d6e2a164e56a301fd0a2b5
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-04-01 12:06:22 -0400

    Adding Easy questions

---
+++ src/js/questions.js
@@ -0,0 +1,230 @@
+const EasyQuestion1 = {
+  questionContent:
+    "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "Molotov-Ribbentrop Pact",
+      correct: true,
+    },
+    {
+      choiceContent: "Agreement of Mutual Assistance",
+      correct: false,
+    },
+    {
+      choiceContent: "Tripartite Pact",
+      correct: false,
+    },
+    {
+      choiceContent: "Treaty of Versailles",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion2 = {
+  questionContent: "Which country did Germany invade that started WW2?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "Denmark",
+      correct: false,
+    },
+    {
+      choiceContent: "Czechoslovakia",
+      correct: false,
+    },
+    {
+      choiceContent: "Poland",
+      correct: true,
+    },
+    {
+      choiceContent: "France",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion3 = {
+  questionContent:
+    "What was the name of the fortified line the French created along the border of Germany prior to WW2?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "The Maginot Line",
+      correct: true,
+    },
+    {
+      choiceContent: "Siegfried Line",
+      correct: false,
+    },
+    {
+      choiceContent: "Gustav Line",
+      correct: false,
+    },
+    {
+      choiceContent: "Mareth Line",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion4 = {
+  questionContent: "Which general during WW2 was known as the “Desert Fox”?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "Bernard Montgomery",
+      correct: false,
+    },
+    {
+      choiceContent: "Erwin Rommel",
+      correct: true,
+    },
+    {
+      choiceContent: "Giovanni Messe",
+      correct: false,
+    },
+    {
+      choiceContent: "Erich von Manstein",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion5 = {
+  questionContent: "What was the Battle of Britain?",
+  value: 1,
+  choices: [
+    {
+      choiceContent:
+        "A naval campaign in the Atlantic Ocean, in which Germany attempted to starve Britain out of the war by destroying convoys carrying imported goods.",
+      correct: false,
+    },
+    {
+      choiceContent:
+        "An amphibious assault where a combined Allied taskforce invaded Normandy in order to open up an invasion on the rest of Axis-occupied Europe.",
+      correct: false,
+    },
+    {
+      choiceContent:
+        "An amphibious evacuation where the British managed to evacuate 300,000 soldiers at Dunkirk back to Britain before they could be destroyed.",
+      correct: false,
+    },
+    {
+      choiceContent:
+        "An aerial campaign over the United Kingdom, in which Germany attempted to destroy Britain’s ability to fight further in the war.",
+      correct: true,
+    },
+  ],
+};
+const EasyQuestion6 = {
+  questionContent: "Which event caused the United States to join WW2?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "The Japanese invasion of China",
+      correct: false,
+    },
+    {
+      choiceContent: "The German usage of unrestricted submarine warfare",
+      correct: false,
+    },
+    {
+      choiceContent: "The Japanese attack on Pearl Harbor",
+      correct: true,
+    },
+    {
+      choiceContent: "The fall of Paris to the Germans",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion7 = {
+  questionContent:
+    "Operation _______  was the codename for the Allied invasion of Normandy in 1944.",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "Overlord",
+      correct: true,
+    },
+    {
+      choiceContent: "Neptune",
+      correct: false,
+    },
+    {
+      choiceContent: "Bagration",
+      correct: false,
+    },
+    {
+      choiceContent: "Market Garden",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion8 = {
+  questionContent:
+    "Which battle in the Eastern Front is considered to be one of the most important turning points of the war?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "The Siege of Sevastapol",
+      correct: false,
+    },
+    {
+      choiceContent: "The Battle of Stalingrad",
+      correct: true,
+    },
+    {
+      choiceContent: "The Battle of Smolensk",
+      correct: false,
+    },
+    {
+      choiceContent: "The Siege of Leningrad",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion9 = {
+  questionContent:
+    "In which Pacific battle did the famous picture of American soldiers raising a flag come from?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "Okinawa",
+      correct: false,
+    },
+    {
+      choiceContent: "Iwo Jima",
+      correct: true,
+    },
+    {
+      choiceContent: "Guadalcanal",
+      correct: false,
+    },
+    {
+      choiceContent: "Philippines",
+      correct: false,
+    },
+  ],
+};
+const EasyQuestion10 = {
+  questionContent: "Which Allied nation was the first to enter Berlin in 1945?",
+  value: 1,
+  choices: [
+    {
+      choiceContent: "USSR",
+      correct: true,
+    },
+    {
+      choiceContent: "United States",
+      correct: false,
+    },
+    {
+      choiceContent: "France",
+      correct: false,
+    },
+    {
+      choiceContent: "United Kingdom",
+      correct: false,
+    },
+  ],
+};
+console.log("connected");
