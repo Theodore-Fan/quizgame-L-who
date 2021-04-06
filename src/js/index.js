57e5a63f88db600829cf8cffc2c4d6ed2ada1746
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-04-06 13:03:54 -0400

    Wait I didn't commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,123 +1,89 @@
-// var myQuestions = [
-//   {
-//     question: "What is 10/2?",
-//     answers: {
-//       a: "3",
-//       b: "5",
-//       c: "115",
-//     },
-//     correctAnswer: "b",
-//   },
-//   {
-//     question: "What is 30/3?",
-//     answers: {
-//       a: "3",
-//       b: "5",
-//       c: "10",
-//     },
-//     correctAnswer: "c",
-//   },
-// ];
-
-// var quizContainer = document.getElementById("quiz");
-// var resultsContainer = document.getElementById("results");
-// var submitButton = document.getElementById("submit");
-
-// generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-// function generateQuiz(
-//   questions,
-//   quizContainer,
-//   resultsContainer,
-//   submitButton
-// ) {
-//   function showQuestions(questions, quizContainer) {
-//     // we'll need a place to store the output and the answer choices
-//     var output = [];
-//     var answers;
-
-//     // for each question...
-//     for (var i = 0; i < questions.length; i++) {
-//       // first reset the list of answers
-//       answers = [];
-
-//       // for each available answer...
-//       for (letter in questions[i].answers) {
-//         // ...add an html radio button
-//         answers.push(
-//           "<label>" +
-//             '<input type="radio" name="question' +
-//             i +
-//             '" value="' +
-//             letter +
-//             '">' +
-//             letter +
-//             ": " +
-//             questions[i].answers[letter] +
-//             "</label>"
-//         );
-//       }
-
-//       // add this question and its answers to the output
-//       output.push(
-//         '<div class="question">' +
-//           questions[i].question +
-//           "</div>" +
-//           '<div class="answers">' +
-//           answers.join("") +
-//           "</div>"
-//       );
-//     }
-
-//     // finally combine our output list into one string of html and put it on the page
-//     quizContainer.innerHTML = output.join("");
-//   }
-
-//   function showResults(questions, quizContainer, resultsContainer) {
-//     // gather answer containers from our quiz
-//     var answerContainers = quizContainer.querySelectorAll(".answers");
-
-//     // keep track of user's answers
-//     var userAnswer = "";
-//     var numCorrect = 0;
-
-//     // for each question...
-//     for (var i = 0; i < questions.length; i++) {
-//       // find selected answer
-//       userAnswer = (
-//         answerContainers[i].querySelector(
-//           "input[name=question" + i + "]:checked"
-//         ) || {}
-//       ).value;
-
-//       // if answer is correct
-//       if (userAnswer === questions[i].correctAnswer) {
-//         // add to the number of correct answers
-//         numCorrect++;
-
-//         // color the answers green
-//         answerContainers[i].style.color = "lightgreen";
-//       }
-//       // if answer is wrong or blank
-//       else {
-//         // color the answers red
-//         answerContainers[i].style.color = "red";
-//       }
-//     }
-
-//     // show number of correct answers out of total
-//     resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
-//   }
-
-//   // show questions right away
-//   showQuestions(questions, quizContainer);
-
-//   // on submit, show results
-//   submitButton.onclick = function () {
-//     showResults(questions, quizContainer, resultsContainer);
-//   };
-// }
 const startButton = document.getElementById("start-btn");
-function startGame() {}
-function setNextQuestion() {}
-function selectAnswer() {}
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+const questions = [
+  {
+    question:
+      "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
+    answers: [
+      { text: "Molotov-Ribbentrop Pact", correct: true },
+      { text: "Agreement of Mutual Assistance", correct: false },
+      { text: "Tripartite Pact", correct: false },
+      { text: "Treaty of Versailles", correct: false },
+    ],
+  },
+];
