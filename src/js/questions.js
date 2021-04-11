69dc88264cab08ff482e82a34e8f9daabae5946e
FrederickLen <71602899+FrederickLen@users.noreply.github.com>
2021-04-11 10:56:48 -0400

    Working now! (Kinda)

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,219 +1,89 @@
-const question = {
-  questionContent:
-    "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
-  choices: [
-    {
-      choiceContent: "Molotov-Ribbentrop Pact",
-      correct: true,
-    },
-    {
-      choiceContent: "Agreement of Mutual Assistance",
-      correct: false,
-    },
-    {
-      choiceContent: "Tripartite Pact",
-      correct: false,
-    },
-    {
-      choiceContent: "Treaty of Versailles",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent: "Which country did Germany invade that started WW2?",
-  choices: [
-    {
-      choiceContent: "Denmark",
-      correct: false,
-    },
-    {
-      choiceContent: "Czechoslovakia",
-      correct: false,
-    },
-    {
-      choiceContent: "Poland",
-      correct: true,
-    },
-    {
-      choiceContent: "France",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent:
-    "What was the name of the fortified line the French created along the border of Germany prior to WW2?",
-  choices: [
-    {
-      choiceContent: "The Maginot Line",
-      correct: true,
-    },
-    {
-      choiceContent: "Siegfried Line",
-      correct: false,
-    },
-    {
-      choiceContent: "Gustav Line",
-      correct: false,
-    },
-    {
-      choiceContent: "Mareth Line",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent: "Which general during WW2 was known as the “Desert Fox”?",
-  choices: [
-    {
-      choiceContent: "Bernard Montgomery",
-      correct: false,
-    },
-    {
-      choiceContent: "Erwin Rommel",
-      correct: true,
-    },
-    {
-      choiceContent: "Giovanni Messe",
-      correct: false,
-    },
-    {
-      choiceContent: "Erich von Manstein",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent: "What was the Battle of Britain?",
-  choices: [
-    {
-      choiceContent:
-        "A naval campaign in the Atlantic Ocean, in which Germany attempted to starve Britain out of the war by destroying convoys carrying imported goods.",
-      correct: false,
-    },
-    {
-      choiceContent:
-        "An amphibious assault where a combined Allied taskforce invaded Normandy in order to open up an invasion on the rest of Axis-occupied Europe.",
-      correct: false,
-    },
-    {
-      choiceContent:
-        "An amphibious evacuation where the British managed to evacuate 300,000 soldiers at Dunkirk back to Britain before they could be destroyed.",
-      correct: false,
-    },
-    {
-      choiceContent:
-        "An aerial campaign over the United Kingdom, in which Germany attempted to destroy Britain’s ability to fight further in the war.",
-      correct: true,
-    },
-  ],
-};
-const question = {
-  questionContent: "Which event caused the United States to join WW2?",
-  choices: [
-    {
-      choiceContent: "The Japanese invasion of China",
-      correct: false,
-    },
-    {
-      choiceContent: "The German usage of unrestricted submarine warfare",
-      correct: false,
-    },
-    {
-      choiceContent: "The Japanese attack on Pearl Harbor",
-      correct: true,
-    },
-    {
-      choiceContent: "The fall of Paris to the Germans",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent:
-    "Operation _______  was the codename for the Allied invasion of Normandy in 1944.",
-  choices: [
-    {
-      choiceContent: "Overlord",
-      correct: true,
-    },
-    {
-      choiceContent: "Neptune",
-      correct: false,
-    },
-    {
-      choiceContent: "Bagration",
-      correct: false,
-    },
-    {
-      choiceContent: "Market Garden",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent:
-    "Which battle in the Eastern Front is considered to be one of the most important turning points of the war?",
-  choices: [
-    {
-      choiceContent: "The Siege of Sevastapol",
-      correct: false,
-    },
-    {
-      choiceContent: "The Battle of Stalingrad",
-      correct: true,
-    },
-    {
-      choiceContent: "The Battle of Smolensk",
-      correct: false,
-    },
-    {
-      choiceContent: "The Siege of Leningrad",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent:
-    "In which Pacific battle did the famous picture of American soldiers raising a flag come from?",
-  choices: [
-    {
-      choiceContent: "Okinawa",
-      correct: false,
-    },
-    {
-      choiceContent: "Iwo Jima",
-      correct: true,
-    },
-    {
-      choiceContent: "Guadalcanal",
-      correct: false,
-    },
-    {
-      choiceContent: "Philippines",
-      correct: false,
-    },
-  ],
-};
-const question = {
-  questionContent: "Which Allied nation was the first to enter Berlin in 1945?",
-  choices: [
-    {
-      choiceContent: "USSR",
-      correct: true,
-    },
-    {
-      choiceContent: "United States",
-      correct: false,
-    },
-    {
-      choiceContent: "France",
-      correct: false,
-    },
-    {
-      choiceContent: "United Kingdom",
-      correct: false,
-    },
-  ],
-};
+const questions = [
+  {
+    question:
+      "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
+    answers: [
+      { text: "Molotov-Ribbentrop Pact", correct: true },
+      { text: "Agreement of Mutual Assistance", correct: false },
+      { text: "Tripartite Pact", correct: false },
+      { text: "Treaty of Versailles", correct: false },
+    ],
+    question:
+      "What was the name of the non-aggression pact that was signed between Germany and the Soviet Union?",
+    answers: [
+      { text: "Molotov-Ribbentrop Pact", correct: true },
+      { text: "Agreement of Mutual Assistance", correct: false },
+      { text: "Tripartite Pact", correct: false },
+      { text: "Treaty of Versailles", correct: false },
+    ],
+
+    question: "Which country did Germany invade that started WW2?",
+    answers: [
+      { text: "Denmark", correct: false },
+      { text: "Czechoslovakia", correct: false },
+      { text: "Poland", correct: true },
+      { text: "France", correct: false },
+    ],
+    question:
+      "What was the name of the fortified line the French created along the border of Germany prior to WW2?",
+    answers: [
+      { text: "The Maginot Line", correct: true },
+      { text: "Siegfried Line", correct: false },
+      { text: "Gustav Line", correct: false },
+      { text: "Mareth Line", correct: false },
+    ],
+    question: "Which general during WW2 was known as the “Desert Fox”?",
+    answers: [
+      { text: "Bernard Montgomery", correct: false },
+      { text: "Erwin Rommel", correct: true },
+      { text: "Giovanni Messe", correct: false },
+      { text: "Erich von Manstein", correct: false },
+    ],
+    question: "What was the Battle of Britain?",
+    answers: [
+      { text: "A naval campaign in the Atlantic Ocean.", correct: false },
+      { text: "An Allied amphibious assault at Normandy.", correct: false },
+      { text: "An amphibious evacuation at Dunkirk.", correct: false },
+      { text: "An aerial campaign over the United Kingdom.", correct: true },
+    ],
+    question: "Which event caused the United States to join WW2?",
+    answers: [
+      { text: "The Japanese invasion of China", correct: false },
+      { text: "Unrestricted submarine warfare by the Germans", correct: false },
+      { text: "The Japanese attack on Pearl Harbor", correct: true },
+      { text: "The fall of Paris to the Germans", correct: false },
+    ],
+    question:
+      "What was the codename for the Allied invasion of Normandy in 1944?",
+    answers: [
+      { text: "Operation Overlord", correct: true },
+      { text: "Operation Neptune", correct: false },
+      { text: "Operation Bagration", correct: false },
+      { text: "Operation Market Garden", correct: false },
+    ],
+    question:
+      "Which battle in the Eastern Front is considered to be one of the most important turning points of the war?",
+    answers: [
+      { text: "The Siege of Sevastapol", correct: false },
+      { text: "The Battle of Stalingrad", correct: true },
+      { text: "The Battle of Smolensk", correct: false },
+      { text: "The Siege of Leningrad", correct: false },
+    ],
+    question:
+      "In which Pacific battle did the famous picture of American soldiers raising a flag come from?",
+    answers: [
+      { text: "Okinawa", correct: false },
+      { text: "Iwo Jima", correct: true },
+      { text: "Guadalcanal", correct: false },
+      { text: "Philippines", correct: false },
+    ],
+    question: "Which Allied nation was the first to enter Berlin in 1945?",
+    answers: [
+      { text: "USSR", correct: true },
+      { text: "United States", correct: false },
+      { text: "France", correct: false },
+      { text: "United Kingdom", correct: false },
+    ],
+  },
+];
+export { questions };
